3f114883aa24a2b917176b6fcd3cbd6d4744edea
Andreas Pandu Pamungkas <andspemail@gmail.com>
2021-11-18 18:07:30 +0700

    Java - Cek String Palindrom

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static boolean isPalindrom(String teks)
+    {
+        int j = teks.length() - 1;
+        int i = 0;
+
+        while (i < j)
+        {
+            if (teks.charAt(i) != teks.charAt(j))
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Teks : ");
+        String teks = scan.nextLine();
+
+        String isPalindrom = isPalindrom(teks) ? "Palindrom" : "Bukan Palindrom";
+
+        System.out.println("Output : " + isPalindrom);
+    }
+}
